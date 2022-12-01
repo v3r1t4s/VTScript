5fcd421af66bbacca8dcd1d330c7c583cb5a096a
v3r1t4s <lemenz.ne555@gmail.com>
2022-11-30 17:13:48 -0700

    Update Script #12

--- script_vts.py
+++ script_vts.py
@@ -4,7 +4,6 @@ import json
 import Helpers
 import argparse
 import requests
-from time import sleep
 from pathlib import Path
 
 ##########################################################
@@ -38,50 +37,49 @@ def search_api_request(key, data):
     response_search_api = requests.get(url, headers=headers, timeout=10)
     if response_search_api.status_code == 200:
         return response_search_api.text
-    if response_search_api.status_code == 400:
-        print(f"[-] HTTP response wasn't successful: {response_search_api.status_code}, made too many request need to wait 60 seconds...")
-        sleep(60)
-        search_api_request(key, data)
-    print(f"[-] HTTP response wasn't successful: {response_search_api.status_code}")
+    print(f"[-] HTTP response wasn't successful: {response_search_api.status_code}, here's the full response we got: \n{response_search_api.text}")
     return None
 
 
-def parse_json(report_data_dic, res_parse_json, string_parse_json):
-    """Parse json response from VT to find malicious signs"""
-    print(f"[*] Working on {string_parse_json}...\n")  # Print the line to separate each parsing when having multiple responses and Printing out the string that's being parsed at this time
-    report_data_dic.append(f"------------------------------------------------------------------------------------- \n\n[*] Checking {string_parse_json}\n")  # Print the line to separate each parsing when having multiple responses and Printing out the string that's being parsed at this time
-    string_parse_json = string_parse_json.replace('\n', '')  # Removing \n character
-    res_dic = json.loads(res_parse_json)  # Loading the output into a dictionnary
-    if res_dic['data'] == []:  # Check if VT returns us something interesting when we query api_req(), If not, return.
-        report_data_dic.append("[-] Sorry but VirusTotal didn't found something...\n")
-        return "Didn't found something", report_data_dic, string_parse_json, res_dic, "", res_parse_json
-    if "text" in res_dic['data'][0]['attributes']:  # text is a unique key (in the dictionnary), only found in the response of a Tag comment specific API query, If we're finding this key return to main
-        report_data_dic.append("[*] Sorry but searching Tag Comment isn't available.\n[*] If you want to search one go there: https://www.virustotal.com/gui/home/search\n")
-        return "Tag comment not available", report_data_dic, string_parse_json, res_dic, "", res_parse_json
-    analysis_stat = res_dic['data'][0]['attributes']['last_analysis_stats']  # Parsing the output for last analysis statistics
-    return "", report_data_dic, string_parse_json, res_dic, analysis_stat, res_parse_json
+def load_json(res_parse_json):
+    """Load json string and convert it into a dictionnary"""
+    res_dic = json.loads(res_parse_json)                                                                                                                                     # Loading the output into a dictionnary
+    return res_dic
 
 
-def data_processing(string_parse_json, res_dic, analysis_stat, res_parse_json, report_data_dic):
+def parse_vt_response(res_dic):
+    """Parse json response from VT to find errors and if none return the response"""
+    if res_dic['data'] == []:                                                                                                                                                # Check if VT returns us something interesting when we query res_dic['data'], If not, return.
+        print("[-] Sorry but VirusTotal didn't found something...\n")
+        return 1, ""
+    if "text" in res_dic['data'][0]['attributes']:                                                                                                                           # text is a unique key (in the dictionnary), only found in the response of a Tag comment specific API query, If we're finding this key return to main
+        print("[*] Sorry but searching Tag Comment isn't available.\n[*] If you want to search one go there: https://www.virustotal.com/gui/home/search\n")
+        return 1,  ""
+    analysis_stat = res_dic['data'][0]['attributes']['last_analysis_stats']
+    return 0, analysis_stat
+
+
+def data_processing(string_parse_json, res_dic, analysis_stat, report_dic):
     """Function used to process data"""
-    report_data_dic.append(f"[*] Latest Analysis Statistics available counting the number of reports: {analysis_stat}\n")
+    report_dic.append(f"-----------------------------------------------------------------------\n[*] Checking {string_parse_json}\n[*] Latest Analysis Statistics available counting the number of reports: {analysis_stat}\n")     # Parsing the output for last analysis statistics
+    string_parse_json = string_parse_json.strip('\n')                                                                                                                                                                               # Removing \n character
     malicious = analysis_stat['malicious']
-    if malicious:  # Checking if malicious key has a hit
-        report_data_dic.append(f"[*] Detailed reports about {string_parse_json} (engine name : result of the analysis): \n")
-        analysis_res = res_dic['data'][0]['attributes']['last_analysis_results']  # Preparing to parse the output for latest analysis results
-        for value in analysis_res.values():  # Parsing the output and checking every analysis made to find malicious one
+    if malicious:                                                                                                                                                                                                                   # Checking if malicious key has a hit
+        report_dic.append(f"[*] Detailed reports about {string_parse_json} (engine name : result of the analysis): \n")
+        analysis_res = res_dic['data'][0]['attributes']['last_analysis_results']                                                                                                                                                    # Preparing to parse the output for latest analysis results
+        for value in analysis_res.values():                                                                                                                                                                                         # Parsing the output and checking every analysis made to find malicious one
             if "malicious" in value['category']:
-                report_data_dic.append(f"[+] {value['engine_name']} : {value['result']}")
-    report_data_dic.append(f"\n[+] You can find the full report from the search request here: \n {res_parse_json} \n[+] {string_parse_json} was flagged {malicious} times.")
-    is_hash = '.' in string_parse_json  # Checking if string provided was a hash
-    if malicious >= 3 and is_hash is not True:
-        report_data_dic.append(f"\n[+] {string_parse_json} is definitely malicious\n")
-        return "", report_data_dic
+                report_dic.append(f"[+] {value['engine_name']} : {value['result']}")
+    report_dic.append(f"\n[+] {string_parse_json} was flagged {malicious} times.")
+    is_string_hash = not ('.' in string_parse_json)                                                                                                                                                                                 # Checking if string provided was a hash
+    if malicious >= 3 and is_string_hash is True:
+        report_dic.append(f"\n[+] {string_parse_json} is definitely malicious\n")
+        return 1, report_dic
     elif malicious:
-        report_data_dic.append(f"\n[+] {string_parse_json} is definitely malicious\n")
+        report_dic.append(f"\n[+] {string_parse_json} is definitely malicious\n")
     else:
-        report_data_dic.append(f"[+] {string_parse_json} doesn't seems to be malicious.\n")
-    return None, report_data_dic
+        report_dic.append(f"[+] {string_parse_json} doesn't seems to be malicious.\n")
+    return 0, report_dic
 
 
 def file_reputation_api_request(key, hash_file_reputation):
@@ -93,27 +91,29 @@ def file_reputation_api_request(key, hash_file_reputation):
     response_file_reputation = requests.get(url, headers=headers, timeout=10)
     if response_file_reputation.status_code == 200:
         return response_file_reputation.text
-    if response_file_reputation.status_code == 400:
-        print(f"[-] HTTP response wasn't successful: {response_file_reputation.status_code}, we made too many request, we need to wait 60 seconds...")
-        sleep(60)
-        file_reputation_api_request(key, hash_file_reputation)
-    print(f"[-] HTTP response wasn't successful: {response_file_reputation.status_code}")
+    print(f"[-] HTTP response wasn't successful: {response_file_reputation.status_code}, here's the full response we got: {response_file_reputation.text}")
     return None
 
 
-def handle_file_report(report_data_dic, string_argument):
+def handle_file_report(report_dic, string_argument):
     """Function used to handle our file report"""
     response = search_api_request(api_key, string_argument)
-    if response is None:  # If we receive nothing quit with error
-        print("[-] We didn't receive something from our search request.")
-        return 1
-    parse_json_error, report_data_dic, string_parse_json, res_dic, analysis_stat, res_parse_json = parse_json(report_data_dic, response, string_argument)
-    if parse_json_error == "":
-        call_to_file_reputation,  report_data_dic = data_processing(string_parse_json, res_dic, analysis_stat, res_parse_json, report_data_dic)
-        if call_to_file_reputation == "":  # check if we need to call for file reputation
+    if response is None:
+        report_dic.append("[-] The search response we got had errors.")
+        return 1, ""
+    print(f"---------------------------------\n[*] Working on {string_argument}\n")
+    response_dictionary = load_json(response)
+    parse_json_error, analysis_stat = parse_vt_response(response_dictionary)
+    if parse_json_error == 0:
+        call_to_file_reputation, report_dic = data_processing(string_argument, response_dictionary, analysis_stat, report_dic)
+        if call_to_file_reputation == 1:
             response_file_reputation = file_reputation_api_request(api_key, string_argument)
-            report_data_dic.append(f"[+] Response from File reputation request: \n {response_file_reputation}")
-    return report_data_dic
+            if response_file_reputation is None:
+                report_dic.append("[-] The file reputation response we got had errors.")
+                return 1, ""
+            response_file_reputation = load_json(response_file_reputation)
+            report_dic.append(f"[+] Getting you data of the file reputation response we got for {string_argument}:\n[*] The type_description of the hash is : {response_file_reputation['data']['attributes']['type_description']}\n[*] The hash was provided to VirusTotal by {str(response_file_reputation['data']['attributes']['unique_sources'])} unique sources\n[*] The most meaningful name of the hash file found by VirusTotal is: {response_file_reputation['data']['attributes']['meaningful_name']}\n[-] Check this link for more infos on your file: https://www.virustotal.com/gui/file/{string_argument}")
+    return 0, report_dic
 
 ##########################################################
 # DATA FORMATTING + FILE WRITE
@@ -134,19 +134,20 @@ def handle_file():
     """This function is used to handle the functions to create our file"""
     prompt_filename = ""
     filename = input_and_parse_filename(prompt_filename, True, '_')
-    report_data_dic = []
-    if file_arg:  # If user launched the script with the file cmdline arg, else then string cmdline argument scenario start
-        data_list = args.file.readlines()  # Create a list with each elements on each line
-        for d in data_list:  # Using for loop to sent each element of the list to the api request function
-            report_data_dic = handle_file_report(report_data_dic, d)
-            if report_data_dic == 1:
+    report_dic = []
+    if file_arg:                                                                        # If user launched the script with the file cmdline arg, else then string cmdline argument scenario start
+        data_list = args.file.readlines()                                               # Create a list with each elements on each line
+        for d in data_list:                                                             # Using for loop to sent each element of the list to the api request function
+            error, report_dic = handle_file_report(report_dic, d)
+            if error == 1:
                 return 1
     else:
-        report_data_dic = handle_file_report(report_data_dic, args.string)
-        if report_data_dic == 1:
+        error, report_dic = handle_file_report(report_dic,  args.string)
+        if error == 1:
             return 1
-    for r in report_data_dic:
+    for r in report_dic:
         append_data_to_a_file(filename, "txt", r)
+
     print(f"[+] The file report can be found as {filename}.txt in {Path.cwd()}")
     return 0
 
@@ -163,8 +164,7 @@ def verify_at_least_x_command_line_arguments(arguments, argument_count):
 
 
 def opening_file_stripping_new_lines(file):
-    """ Checking if the file is well formatted (no blank line, a string on each line),
-    if not format the file"""
+    """ Checking if the file is well formatted (no blank line, a string on each line), if not format the file"""
     try:
         with open(file, encoding="UTF-8") as in_file, open(file, 'r+', encoding="UTF-8") as out_file:
             out_file.writelines(line for line in in_file if line.strip())
@@ -186,6 +186,17 @@ def get_path_from_user(prompt, text_for_input):
         return None
     return prompt
 
+
+def counting_lines_of_file(filename_to_count_to):
+    """Function used to count number of lines in a file"""
+    try:
+        with open(filename_to_count_to, "r", encoding="UTF-8") as file:
+            nb_of_lines = len(file.readlines())
+            return nb_of_lines
+    except IOError as io_err:
+        Helpers.exception_handler(True, io_err)
+    return None
+
 ##########################################################
 # MAIN AND SETUP
 ##########################################################
@@ -193,36 +204,80 @@ def get_path_from_user(prompt, text_for_input):
 
 def parse_arguments():
     """Function used to parse arguments"""
-    parser = argparse.ArgumentParser()  # Create the parser to have arguments in the script
-    parser.add_argument('--string', type=str, )  # Adding string argument
+    parser = argparse.ArgumentParser()                          # Create the parser to have arguments in the script
+    parser.add_argument('--string', type=str, )                 # Adding string argument
     parser.add_argument('--file', type=argparse.FileType('r'))  # Adding File argument
-    return parser.parse_args()  # Parse the argument
+    return parser.parse_args()                                  # Parse the argument
+
+
+def get_user_quota_summary(key):
+    """Function used to query VT API about our quota"""
+    url = f"https://www.virustotal.com/api/v3/users/{key}/overall_quotas"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": key
+    }
+    response_get_user_quota_summary = requests.get(url, headers=headers, timeout=10)
+    if response_get_user_quota_summary.status_code == 200:
+        return response_get_user_quota_summary.text
+    print(f"[-] HTTP response wasn't successful: {response_get_user_quota_summary.status_code}, here's the full response we got: {response_get_user_quota_summary.text}")
+    return None
+
+
+def check_quota(quota_json, nb_requests_user_want):
+    """Function used to check if we respect the quota"""
+    quota_dic = load_json(quota_json)
+    quota_monthly = quota_dic['data']['api_requests_monthly']['user']['allowed'] - quota_dic['data']['api_requests_monthly']['user']['used']
+    if nb_requests_user_want > quota_monthly:
+        print("[-] You already used your quota for the month come back later...")
+        return None
+    quota_daily = quota_dic['data']['api_requests_daily']['user']['allowed'] - quota_dic['data']['api_requests_daily']['user']['used']
+    if nb_requests_user_want > quota_daily:
+        print("[-] You already used your quota for the day come back later...")
+        return None
+    quota_hourly = quota_dic['data']['api_requests_hourly']['user']['allowed'] - quota_dic['data']['api_requests_hourly']['user']['used']
+    if nb_requests_user_want > quota_hourly:
+        print("[-] You already used your quota for the hour come back later...")
+        return None
+    return ""
 
 
 if __name__ == '__main__':
-    if verify_at_least_x_command_line_arguments(sys.argv, 1) is None:  # Check if an argument is provided
+    if verify_at_least_x_command_line_arguments(sys.argv, 1) is None:                                         # Check if an argument is provided
         print("[-] Please provide at least one command line argument(s)")
         exit(1)
 
     args = parse_arguments()
 
-    file_arg = False  # Set file argument to false until we know there's one
+    file_arg = False                                                                                          # Set file argument to false until we know there's one
 
-    if args.file:  # Set to true if we find that it is used as cmdline argument, Remove any blank line so we can send proper string to the api_req()
+    number_of_arguments_to_check = 1
+
+    if args.file:                                                                                             # Set to true if we find that it is used as cmdline argument, Remove any blank line so we can send proper string to the api_req()
         file_arg = True
-        opening_file_stripping_new_lines(sys.argv[2])
+        file_name = sys.argv[2]
+        opening_file_stripping_new_lines(file_name)
+        number_of_arguments_to_check = counting_lines_of_file(file_name)                                      # Counting the number of lines if we provide a file
 
     string = ""
 
     path = get_path_from_user(string, "Please input the absolute path of your API key configuration file: ")  # Call the get_path function to have the path of the API Key File
 
-    if path is None:  # If we receive nothing quit with error
+    if path is None:                                                                                          # If we receive nothing quit with error
         exit(1)
 
-    api_key = Helpers.read_api_key(path)  # Call the read_api_key function to read the API Key from the file
+    api_key = Helpers.read_api_key(path)                                                                      # Call the read_api_key function to read the API Key from the file
 
-    if api_key == '':  # If we receive nothing quit with error
+    if api_key == '':                                                                                         # If we receive nothing quit with error
         print("[-] We couldn't find your API Key.")
+        exit(1)
+
+    response_quota = get_user_quota_summary(api_key)                                                          # Requesting the API for a quota usage summary
+
+    if response_quota is None:
+        exit(1)
+
+    if check_quota(response_quota, number_of_arguments_to_check) is None:                                     # Calling check quota to check if the number of arguments we want to use for our API requests is greater than the quota available
         exit(1)
 
     if handle_file() == 0:
