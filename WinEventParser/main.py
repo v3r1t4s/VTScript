d57cdf9b4230c79aff1299fd10e5fb95c2402679
v3r1t4s <lemenz.ne555@gmail.com>
2023-02-13 11:42:10 -0700

    Update Script WinEventParser #3

--- WinEventParser/main.py
+++ WinEventParser/main.py
@@ -1,12 +1,10 @@
 """import of modules"""
-import re
-import json
 import ctypes
 import Helpers
 import datetime
+import WinParser
+import WinAnalysis
 from dateutil import tz
-import win32evtlog
-import xml.etree.ElementTree as ET
 
 
 def admin():
@@ -26,7 +24,7 @@ def is_valid_char(char):
     return char.isdigit() or char in acceptable_chars
 
 
-def get_chars_to_add(mode):
+def get_string_to_add_to_timestamp(mode):
     """Function used to return the string to add to our timestamp"""
     chars_to_add = ""
     if mode == 2:
@@ -50,7 +48,7 @@ def parse_timestamp(timestamp_to_be_parsed, mode):
     if not all(is_valid_char(c) for c in timestamp_to_be_parsed):
         return None, None
     try:
-        chars_to_add = get_chars_to_add(mode)
+        chars_to_add = get_string_to_add_to_timestamp(mode)
         len_to_remove = len(chars_to_add)
         timestamp_parsed = datetime.datetime.strptime(timestamp_to_be_parsed + chars_to_add, "%Y-%m-%d %H:%M:%S")
     except ValueError:
@@ -85,14 +83,33 @@ def get_input_mode(iteration_of_for_loop):
     return int(mode_to_pass)
 
 
+def second_timestamp_not_greater_than_first_one(timestamp_from_check_function):
+    """Function used to check that the second timestamp provided is not greater than the first one"""
+    if timestamp_from_check_function[0] > timestamp_from_check_function[1]:
+        return True
+    return False
+
+
+def checking_timestamp(timestamps_provided):
+    """Implementing our logic in this function to check if second timestamp is not greater than first one"""
+    if len(timestamps_provided) == 2:
+        if second_timestamp_not_greater_than_first_one(timestamps_provided) is True:
+            return None
+        else:
+            return ""
+    else:
+        return ""
+
+
 def get_input_timestamp(input_mode_timestamp):
     """Function used to get the timestamp the user want to check the events"""
     try:
         num_of_timestamps = get_num_of_timestamps(input_mode_timestamp)
         timestamp_utc = []
         len_timestamp_input = []
-        iteration_of_for_loop = 1
-        for _ in range(num_of_timestamps):
+        iteration_of_for_loop = 0
+        while iteration_of_for_loop != num_of_timestamps:
+            iteration_of_for_loop += 1
             input_mode = get_input_mode(iteration_of_for_loop)
             timestamp_parsed = None
             while timestamp_parsed is None:
@@ -103,26 +120,13 @@ def get_input_timestamp(input_mode_timestamp):
             timestamp_utc.append(timestamp_parsed)
             if timestamp_utc is None:
                 return None
-            iteration_of_for_loop += 1
-        print(timestamp_utc, len_timestamp_input)
+            if num_of_timestamps == 2 and checking_timestamp(timestamp_utc) is None:
+                print("[-] FIRST TIMESTAMP YOU INPUTED WAS GREATER THAN THE SECOND ONE, PLEASE INPUT BOTH TIMESTAMPS AGAIN")
+                iteration_of_for_loop = 0
         return timestamp_utc, len_timestamp_input
     except ValueError as val_err:
         Helpers.exception_handler(True, val_err)
         return None
-
-# Maybe impplement a logic to be sure that the second timestamp provided
-# (in case the user want to input two) is greater than the first one
-
-
-def load_events_list_json(json_event_file):
-    """Read API Key from the file path provided in get_path()"""
-    try:
-        with open(json_event_file, "r", encoding="UTF-8") as json_file:
-            data_event_file_json = json.load(json_file)
-            return data_event_file_json['events']
-    except IOError as io_err:
-        Helpers.exception_handler(True, io_err)
-    return None
 
 
 def get_time_back_in_local(utc_time):
@@ -133,26 +137,6 @@ def get_time_back_in_local(utc_time):
     utc = utc.replace(tzinfo=from_zone)                                 # Tell the datetime object that it's in UTC time zone since datetime objects are 'naive' by default
     local_time = utc.astimezone(to_zone)                                # Convert time zone
     return local_time
-
-
-def checking_rules_id(event_id_sample, time_created_sample, xml_sample, event_l):
-    """sample function used to check rules list with selected events"""
-    len_event_list = len(event_l)
-    for rule in range(0, len_event_list):
-        if int(event_id_sample) == event_l[rule]['ID']:
-            time_created_sample = time_created_sample.split('.', 1)[0]
-            time_created_sample = time_created_sample.replace('T', ' ')
-            time_created_sample = get_time_back_in_local(time_created_sample)
-            event_data_name_sample = xml_sample.find(f'.//{ns}EventData')                                                                                           # Get the EventData part of our event
-            try:
-                for event_d_name_s in event_data_name_sample:
-                    # print(d.tag, d.attrib, d.text)
-                    if "TargetUserName" in str(event_d_name_s.attrib):                                                                                              # Check if it's credentials related like log on log off or special login
-                        print(f"[*] The event ID ({event_id_sample}) matched our rule list, the description of the rule is: {event_l[rule]['name']:}")
-                        print(f"[+] The TargetUsernName that interact with the machine was: {event_d_name_s.text}, it was registered at: {time_created_sample}")
-            except TypeError as t_error:
-                Helpers.exception_handler(True, t_error)
-                return None
 
 
 if __name__ == '__main__':
@@ -191,66 +175,38 @@ if __name__ == '__main__':
                 timestamp[n_t_f] = timestamp_temp[n_t_f]
                 number_of_occurences[n_t_f] = len_to_remove_to_timestamp[n_t_f]
 
-    event_list = load_events_list_json("EventIdAndNames.json")
+    if input_provided != "all":
+        print("[*] Launching our engine and starting to search events, please wait...")
+        interesting_event_list = WinParser.WinParserMain(input_provided, timestamp, number_of_occurences)
+        print("[+] Parsing is done.")
+        if interesting_event_list is None:
+            print("[-] The checking rules engine return an exception.")
+            exit(1)
+        
+        print("[*] Launching the analysis engine, please wait...")
+        interesting_event_list = WinAnalysis.WinParserMain(interesting_event_list)
+        print("[+] Analysis is done.")
 
-    if event_list is None:
-        print("[-] The file provided in the script can't be open.")
-        exit(1)
+        if interesting_event_list is None:
+            print("[-] The Analysis engine return an exception.")
+            exit(1)
 
-    query_handle = win32evtlog.EvtQuery(r'C:\Windows\System32\winevt\Logs\Security.evtx', win32evtlog.EvtQueryFilePath)  # open event file
-
-    read_count = 0
-    display_only_one_event = True
-    count_matched_event = 0
-
-    while display_only_one_event is True:
-        # display_only_one_event = False                                                                                                                               # read 1 record(s)
-        events = win32evtlog.EvtNext(query_handle, 1)
-        read_count += len(events)
-        if len(events) == 0:                                                                                                                                           # if there is no record break the loop
-            break
-
-        for event in events:
-            xml_content = win32evtlog.EvtRender(event, win32evtlog.EvtRenderEventXml)
-
-            xml = ET.fromstring(xml_content)                                                                                                                           # parse xml content
-
-            # print(xml_content)                                                                                                                                       # Print the whole xml event
-
-            ns = '{http://schemas.microsoft.com/win/2004/08/events/event}'                                                                                             # xml namespace, root element has a xmlns definition, so we have to use the namespace
-
-            event_id = xml.find(f'.//{ns}EventID').text
-            computer = xml.find(f'.//{ns}Computer').text
-            channel = xml.find(f'.//{ns}Channel').text
-            execution = xml.find(f'.//{ns}Execution')
-            process_id = execution.get('ProcessID')
-            thread_id = execution.get('ThreadID')
-            time_created = xml.find(f'.//{ns}TimeCreated').get('SystemTime')
-
-            event_data = f"Time: {time_created}, Computer: {computer}, Event Id: {event_id}, Channel: {channel}, Process Id: {process_id}, Thread Id: {thread_id}"
-
-            if input_provided != "all":
-                time_from_utc_to_local_zone = time_created.split('.', 1)
-                time_from_utc_to_local_zone = time_from_utc_to_local_zone[0]
-                time_from_utc_to_local_zone = time_from_utc_to_local_zone.replace('T', ' ')
-                time_from_utc_to_local_zone = str(get_time_back_in_local(time_from_utc_to_local_zone))
-                search_date_start = re.search((timestamp[0] + ".{"+f"{number_of_occurences[0]}"+"}"), time_from_utc_to_local_zone)
-
-                search_date_end = re.search((timestamp[1] + ".{"+f"{number_of_occurences[1]}"+"}"), time_from_utc_to_local_zone)
-                if search_date_start:
-                    if search_date_end:
-                        break
-                    f = open("FILE.txt", "a", encoding="UTF-8")
-                    f.write(time_created + "\n")
-                    f.close()
-                    count_matched_event += 1
-                    print(f"[*] (Event Nb {count_matched_event} found in the timestamp provided): " + event_data)
-                    if checking_rules_id(event_id, time_created, xml, event_list) is None:
-                        print("[-] This event wasn't interesting")
-
-            else:
-                f = open("FILE.txt", "a", encoding="UTF-8")
-                f.write(time_created + "\n")
-                f.close()
-                print(event_data)
+        # f"[*] (Event Nb {count_matched_event} found in the timestamp provided): " +
+        data_to_append = ""
+        if interesting_event_list == []:
+            data_to_append = "[+] Your system seems clean! The analysis engine didn't return any events!"
+        else:
+            for i in interesting_event_list:
+                data_to_append += i + "\n"
+        print("[*] Writing a report...")
+        try:
+            with open("REPORT.txt", "w", encoding="UTF-8") as file:
+                file.write(data_to_append + "\n")
+                print("[+] You report is done!")
+        except IOError as io_err:
+            Helpers.exception_handler(True, io_err)
+            print("[-] We couldn't write you a report.")
+            exit(1)
+    else:
+        WinParser.WinParserMain(input_provided, None, None)
     exit(0)
